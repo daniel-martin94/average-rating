ee8f808a93bbea4095357f35e1819dd3c51774fe
daniel-martin94 <danielpmartin94@gmail.com>
2020-05-19 15:00:56 -0700

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
+
+import data from './data.json'
 
 
 /*
@@ -9,24 +11,33 @@ import './style.css';
 Given a list of starting ratings, create a compnetnet that allows a user to rate and update the average
 
 */
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
 
-  render() {
+//First start with a displaying the current movie
+function App() {
+  console.log(data.movies)
+
+  const [currIndex, updateIndex] = useState(0)
+  const [currMovie, updateMovie] = useState(data.movies[0])
+
+  function setIndex (idx) {
+    //console.log(idx)
+    // if(idx < 0) {
+    //   console.log("hey")
+    //   updateIndex(data.movies.length - 1)
+    //   console.log(data.movies.length - 1)
+    // //   updateMovie(currIndex)
+    // }
+  }
     return (
       <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+      <button onClick={setIndex(currIndex - 1)}>Decrement</button>
+        <div class="center">
+          <img className="movie-thumbnail" src={currMovie.image}/>
+          <h2>{currMovie.title}</h2>
+        </div>
+        <button onClick={setIndex(currIndex + 1)}>Increment</button>
       </div>
     );
   }
-}
 
 render(<App />, document.getElementById('root'));
